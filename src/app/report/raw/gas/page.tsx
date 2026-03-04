7926bbfbd7b5f7af98b4f63ac0e96ed492404cd6
Alief Muhammad Abdillah <aliefmabdillah09@gmail.com>
2026-03-04 10:23:31 +0700

    fix: error file not found

--- src/app/report/raw/gas/page.tsx
+++ src/app/report/raw/gas/page.tsx
@@ -1,15 +1,10 @@
 import React from "react";
 import LayoutReportFixed from "@/app/components/LayoutReport";
-import LayoutReportMini from "@/app/components/LayoutReportMinit";
 
 export default function ReportPage() {
   return (
     <>
-      {process.env.AQMS_TYPE === "fixed" ? (
-        <LayoutReportFixed param="gas"/>
-      ) : (
-        <LayoutReportMini />
-      )}
+      <LayoutReportFixed param="gas" />
     </>
   );
 }
--- src/app/report/raw/pm/page.tsx
+++ src/app/report/raw/pm/page.tsx
@@ -1,15 +1,9 @@
 import LayoutReportFixed from "@/app/components/LayoutReport";
-import LayoutReportMini from "@/app/components/LayoutReportMinit";
-import React from "react";
 
 export default function ReportPage() {
   return (
     <>
-      {process.env.AQMS_TYPE === "fixed" ? (
-        <LayoutReportFixed param="pm"/>
-      ) : (
-        <LayoutReportMini />
-      )}
+      <LayoutReportFixed param="pm" />
     </>
   );
 }
